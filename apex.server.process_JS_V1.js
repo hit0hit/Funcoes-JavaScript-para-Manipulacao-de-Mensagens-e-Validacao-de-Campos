959ba3bf639e1712115a77c9b8d46483cad8328d
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2023-08-18 13:59:06 -0300

    Update apex.server.process_JS_V1.js

--- apex.server.process_JS_V1.js
+++ apex.server.process_JS_V1.js
@@ -1,2 +1,3 @@
 javascript: apex.item('P16_APAGAR').setValue('#ID_ARQUIVOS#');      
 apex.server.process("PROCESS_APAGAR",         {             x01: "#ID_ARQUIVOS#",             pageItems: "#P16_APAGAR"         },         {             dataType: 'text'         }); 
+apex.region("Material").refresh();
