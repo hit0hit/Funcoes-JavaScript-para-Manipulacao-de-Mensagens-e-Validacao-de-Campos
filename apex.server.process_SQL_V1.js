6e8a02de367f91ebac60bbce3565e037ce60cad0
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2023-08-18 13:43:53 -0300

    Create apex.server.process_SQL_V1.js

---
+++ apex.server.process_SQL_V1.js
@@ -0,0 +1,2 @@
+javascript: apex.item('P16_APAGAR').setValue('#ID_ARQUIVOS#');      
+apex.server.process("PROCESS_APAGAR",         {             x01: "#ID_ARQUIVOS#",             pageItems: "#P16_APAGAR"         },         {             dataType: 'text'         }); 
