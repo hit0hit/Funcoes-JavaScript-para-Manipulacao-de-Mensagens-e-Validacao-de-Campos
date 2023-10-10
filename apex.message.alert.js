d7c400824e5d4b567e07c63bbf9104a2097d90d8
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2023-10-10 11:21:51 -0300

    Update apex.message.alert.js

--- apex.message.alert.js
+++ apex.message.alert.js
@@ -7,4 +7,45 @@ function show_notification(Msg){
         $('#t_Alert_Success div div.t-Alert-icon span').removeClass('t-Icon').addClass('fa fa-2x fa-warning u-danger-text');
 }
 
+//////////////////////////////////////////////////
 show_notification("The correct answer is: "  + apex.item( "P2_SERVICO" ).displayValueFor( "APPLES" ) );
+
+
+
+/////////////////////////////////////////////////////
+apex.message.clearErrors();
+if (apex.item("P2_SERVICO").isEmpty()) {
+    apex.message.showErrors([
+        {
+            type: "error",
+            location: ["page", "inline"],
+            pageItem: "P2_SERVICO",
+            message: "O nome é obrigatório!",
+            unsafe: false
+        }
+    ]);
+}
+
+if (apex.item("P2_QTD").isEmpty()) {
+    apex.message.showErrors([
+        {
+            type: "error",
+            location: ["page", "inline"],
+            pageItem: "P2_QTD",
+            message: "A quantidade é obrigatória!",
+            unsafe: false
+        }
+    ]);
+}
+
+if (apex.item("P2_VALOR").isEmpty()) {
+    apex.message.showErrors([
+        {
+            type: "error",
+            location: ["page", "inline"],
+            pageItem: "P2_VALOR",
+            message: "O valor é obrigatório!",
+            unsafe: false
+        }
+    ]);
+}
